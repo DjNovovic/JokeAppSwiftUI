29f50d627a6a3b1e4b62f8713191255e39f0ab14
DjNovovic <djordjijenovovic@gmail.com>
2021-01-10 12:50:26 +0100

    Initial Commit

---
+++ JokeAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JokeAppSwiftUI
+//
+//  Created by Đorđije Novović on 10.1.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JokeAppSwiftUI/JokeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAppSwiftUIApp.swift
+//  JokeAppSwiftUI
+//
+//  Created by Đorđije Novović on 10.1.21..
+//
+
+import SwiftUI
+
+@main
+struct JokeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
